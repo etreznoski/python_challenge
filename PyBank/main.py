0b45df8637f475b2c5666cf4c71465ba3cc61905
etreznoski <sanchez.elizabethk@gmail.com>
2021-01-11 15:41:27 -0800

    set up lists for dates and profits/losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,11 @@ import csv
 #set up path to csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+#set empty lists
+total_months = []
+profit_losses = []
+
+
 #read CSV file
 with open(csvpath, newline='') as csvfile:
     #specify deliminator and variable to hold contents
@@ -16,6 +21,16 @@ with open(csvpath, newline='') as csvfile:
     csv_header = next(csvreader)
     #print(csv_header)
 
+    #fill in list contents
+    for row in csvreader:
+
+         total_months.append(row[0])
+         profit_losses.append(row[1])
+
+    #test to see if prints total number of months
+    print(len(total_months))
     
+
+
 
 
