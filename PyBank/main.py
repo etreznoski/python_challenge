010dec50fdbb1fb46b3e89aee01bc4551e91746e
etreznoski <sanchez.elizabethk@gmail.com>
2021-01-11 16:30:41 -0800

    removed profit_losses as list. added commented out list of end results

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,8 +7,6 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 
 #set empty lists
 total_months = []
-profit_losses = []
-
 
 #read CSV file
 with open(csvpath, newline='') as csvfile:
@@ -16,20 +14,42 @@ with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     #print(csvreader)
-
-    #read header row
+ #read header row
     csv_header = next(csvreader)
     #print(csv_header)
+    
 
     #fill in list contents
     for row in csvreader:
 
          total_months.append(row[0])
-         profit_losses.append(row[1])
+
+         profit_losses = float(row[1])
+         
+
 
     #test to see if prints total number of months
-    print(len(total_months))
-    
+    # print(f"Total Months:{len(total_months)}")
+
+    net_profit = sum(profit_losses)
+    print(f"Net Profit: {net_profit}")
+
+
+
+
+##--------------------------------------------------------
+## End Result
+
+## print to terminal
+
+# print("Finantial Anlaysis")
+# print("-------------------------------")
+# print(f"Total Months: {len(total_months)}")
+# print(f"Total: ${}")
+# print(f"Average Change: ${}")
+# print(f"Greatest Increase in Profits: {}")
+# print(f"Greatest Decrease in Profits: {}")
+
 
 
 
