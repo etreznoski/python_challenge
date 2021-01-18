7333896cee6f59abd4796e480998a81d1efc8868
etreznoski <sanchez.elizabethk@gmail.com>
2021-01-18 11:27:39 -0800

    Pybank: found profit change, stored in list, calculated sum

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,8 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 
 #set empty lists/variables
 total_months = []
-profit_losses = 0
+profit_losses =[]
+total_profit_losses = 0
 
 #read CSV file
 with open(csvpath, newline='') as csvfile:
@@ -24,24 +25,24 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
 
          total_months.append(row[0])
-
-         profit_losses += int(row[1])
-
-         
-
-
-    #test to see if prints total number of months
-    # print(f"Total Months:{len(total_months)}")
-
-    # net_profit = sum(profit_losses)
-    # print(f"Net Profit: {net_profit}")
-
-#calculate the net profit by summing all profit/losses
+         profit_losses.append(int(row[1]))
+         total_profit_losses += int(row[1])
+        
 
 #calculate changes in profit/losses over entire period
 #THen find average of those changes
     #calculate change from one month to the next-store in new list
-    #find mean of new list
+   
+profit_change = []
+
+for x in range(1, len(profit_losses)):
+    y = profit_losses[x] - profit_losses[x-1]
+    profit_change.append(int(y))
+
+#print(profit_change)
+#find mean of new list
+
+
     #find greatest value in new list-grab corresponding date
     #find lowest value in new list-grab corresponding date
 
@@ -54,7 +55,7 @@ with open(csvpath, newline='') as csvfile:
 print("Finantial Anlaysis")
 print("-------------------------------")
 print(f"Total Months: {len(total_months)}")
-print(f"Total: ${profit_losses}")
+print(f"Total: ${total_profit_losses}")
 # print(f"Average Change: ${}")
 # print(f"Greatest Increase in Profits: {}")
 # print(f"Greatest Decrease in Profits: {}")
